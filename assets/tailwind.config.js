4df81460758cc3f9e5b313a548234697b0ff2259
Jose Esteves <jose@ceibo.co>
2021-02-26 20:12:58 -0300

    tailwind custom color testing teal)

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
     '../lib/**/*.ex',
     '../lib/**/*.leex',
     '../lib/**/*.eex',
-    './js/**/*.js'],
+    './js/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: {
+          100: '#75D6D2',
+          200: '#4ECAC5',
+          // DEFAULT: '#44B3AF',
+          DEFAULT: '#49BFB5',
+          300: '#35B0AB',
+          400: '#298985',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
