368c9c5dd6893d7f80b835817064a06c8b483c4b
“Nico” <nicocirio@gmail.com>
2021-08-26 17:15:55 -0300

    minor text changes in landing

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,52 @@ import NProgress from 'nprogress'
 import { LiveSocket } from 'phoenix_live_view'
 
 
+
+
+
 let Hooks = {}
+
+//  Carousel JS logic
+Hooks.Carousel = {
+  mounted() {
+    window.carouselHook = this
+    var slideIndex = 2;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
+
+  },
+  plusSlides(n) {
+    console.log(n)
+    showSlides(slideIndex += n);
+  }
+
+}
+// end Carousel logic
 
 Hooks.AnimateOnIntersect = {
   mounted() {
