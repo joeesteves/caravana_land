35356ac586a499ab57a4027c906187518a8ce460
Jose Esteves <jose@ceibo.co>
2021-08-26 19:07:40 -0300

    sections

--- lib/ceibo_land_web/views/shared_view.ex
+++ lib/ceibo_land_web/views/shared_view.ex
@@ -1,4 +1,7 @@
 defmodule CeiboLandWeb.SharedView do
   use CeiboLandWeb, :view
 
+  def render_section(section_name, assigns \\ []) do
+    render(__MODULE__, "sections/#{section_name}", assigns)
+  end
 end
