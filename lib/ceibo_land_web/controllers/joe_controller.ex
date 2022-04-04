24f323418ac215905a64f3b6f614a12a030edca9
Joe Esteves <joe@caravana.dev>
2022-04-04 14:21:07 -0300

    profile structure

--- lib/ceibo_land_web/controllers/joe_controller.ex
+++ lib/ceibo_land_web/controllers/joe_controller.ex
@@ -1,6 +1,7 @@
 defmodule CeiboLandWeb.JoeController do
   use CeiboLandWeb, :controller
-  plug :put_root_layout, {CeiboLandWeb.LayoutView, "blank.html"}
+  plug :put_root_layout, false
+  plug :put_layout, {CeiboLandWeb.LayoutView, "blank.html"}
 
   def index(conn, _params) do
     render(conn, "index.html")
